a09b02f5a424f8d5c34919660e39c0262f22717c
Ammar SA <ammar.ahmad.sameed@gmail.com>
2025-09-24 14:27:17 +0300

    course 2

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Message {
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+
+}
