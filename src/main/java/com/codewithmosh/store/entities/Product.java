d7a7cebd51080538999c0c92caf255171d661ab9
Ammar SA <ammar.ahmad.sameed@gmail.com>
2025-12-28 00:12:43 +0300

    updating the product entity to add image

--- src/main/java/com/codewithmosh/store/entities/Product.java
+++ src/main/java/com/codewithmosh/store/entities/Product.java
@@ -35,6 +35,9 @@ public class Product {
     @JoinColumn(name = "seller_id", nullable = false)
     private User seller;
 
+    @Column(name = "image")
+    private String imageUrl;
+
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "category_id")
     private Category category;
