dc0f363361009fce1715dd2f2fe5738580cb2110
Ammar SA <ammar.ahmad.sameed@gmail.com>
2025-12-28 23:49:23 +0300

    allowing the frontend

--- src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4242", "https://appstorecourse2-production.up.railway.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4242","https://mr-amsora.github.io/store-front-end/", "https://appstorecourse2-production.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
